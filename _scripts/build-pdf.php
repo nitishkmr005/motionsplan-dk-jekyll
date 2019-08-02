ee9d35507da6692058690691f4e23397eef6eac8
Lars Olesen <lars@intraface.dk>
2019-08-02 06:51:25 +0000

    Initial work on the pdfs

--- _scripts/build-pdf.php
+++ _scripts/build-pdf.php
@@ -7,18 +7,6 @@ use Motionsplan\Exercise\Pdf\Portrait;
 use Motionsplan\Exercise\ExerciseInterface;
 use Motionsplan\Exercise\ExerciseImageInterface;
 
-$filename = __DIR__ . "/../_exercises/180-jump.md";
-
-$str = file_get_contents($filename);
-
-$parser = new Mni\FrontYAML\Parser();
-
-$document = $parser->parse($str);
-
-$yaml = $document->getYAML();
-$html = $document->getContent();
-
-
 class ExerciseImage implements ExerciseImageInterface
 {
     protected $path;
@@ -73,7 +61,7 @@ class ExerciseAdapter implements ExerciseInterface
      */
     public function getImages()
     {
-        
+        if (!is_array($this->document->getYaml()['gallery'])) return array();
         $images = array();
         foreach ($this->document->getYaml()['gallery'] as $img) {
             $images[] = new ExerciseImage(__DIR__ . "/../" . $img['image_path']);
@@ -85,16 +73,38 @@ class ExerciseAdapter implements ExerciseInterface
     {
         return null;
     }
+    
+    function __destruct()
+    {
+        $this->document = null;
+    }
 }
 
 
-$name = pathinfo($filename);
-
-$filename =  __DIR__ . '/../assets/pdf/exercises/' . $name['filename'] . '.pdf';
-$pdf = new Portrait();
-$pdf->setTemporaryDirectory(__DIR__);
-$pdf->setLogo(new ExerciseImage(__DIR__ . '/mp-logo.png'), 'http://motionsplan.dk');
-$pdf->setContribLogo(new ExerciseImage(__DIR__ . '/vih_logo.jpg'), 'http://vih.dk');
-$pdf->addNewPage(new ExerciseAdapter($document));
-// This is not really testing the library - just to see whether functions works.
-$pdf->Output($filename, 'F');
+//Get a list of file paths using the glob function.
+$fileList = glob(__DIR__ . "/../_exercises/*");
+ 
+//Loop through the array that glob returned.
+foreach ($fileList as $filename){
+    $str = file_get_contents($filename);
+    $name = pathinfo($filename);
+    
+    $parser = new Mni\FrontYAML\Parser();
+    $document = $parser->parse($str);
+    
+    unset($parser);
+    
+    $filename =  __DIR__ . '/../assets/pdf/exercises/' . $name['filename'] . '.pdf';
+    $pdf = new Portrait();
+    $pdf->setTemporaryDirectory(__DIR__);
+    $pdf->setLogo(new ExerciseImage(__DIR__ . '/../assets/images/mp-logo.png'), 'http://www.motionsplan.dk');
+    $pdf->setContribLogo(new ExerciseImage(__DIR__ . '/../assets/images/vih_logo.jpg'), 'http://www.vih.dk');
+    $pdf->addNewPage(new ExerciseAdapter($document));
+    // This is not really testing the library - just to see whether functions works.
+    $pdf->Output($filename, 'F');
+    
+    unset($document);
+    unset($pdf);
+    
+    flush();
+}
