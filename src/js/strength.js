d54011c96ae0698e62d62a874e88a1ec16f094cf
Lars Olesen <lars@intraface.dk>
2019-11-15 19:35:36 +0000

    Added Nuckols Index

--- src/js/strength.js
+++ src/js/strength.js
@@ -7,11 +7,31 @@ motionsplan.Strength = function(weight, body_weight) {
   function getIndex100() {
     var Loeft = weight;
     var Vaegt = body_weight;
-    return Math.round(Loeft * 986.63 / (1270.4 - 172970 * ((Math.pow(Vaegt, -1.3925)))) * Math.pow(10, 0)) / Math.pow(10, 0)
+    return Math.round(Loeft * 986.63 / (1270.4 - 172970 * ((Math.pow(Vaegt, -1.3925)))) * Math.pow(10, 0)) / Math.pow(10, 0);
+  }
+  
+  function getAllometricScaling() {
+    var S = weight;
+    var M = body_weight;
+    return S * Math.pow(M, -2/3);
+  }
+
+  function getBodyweightPercent() {
+    return weight / body_weight;
+  }
+  
+  function getNuckolsIndex() {
+    var a, b;
+    var w = weight;
+    var bw = body_weight;
+    
+    return 100*w/(a*Math.pow(bw, 2)+b*bw);
   }
 
   var publicAPI = {
-    getIndex100: getIndex100
+    getIndex100 : getIndex100,
+    getAllometricScaling : getAllometricScaling,
+    getBodyweightPercent : getBodyweightPercent
   };
 
   return publicAPI;
--- src/test/strength.js
+++ src/test/strength.js
@@ -9,4 +9,24 @@ describe('Strength', function() {
       assert.equal(rm.getIndex100(), 89);
     });
   });
+  describe('getAllometricScaling', function() {
+    it('should return the correct number', function() {
+      // weigt, reps
+      var rm = motionsplan.Strength(405, 220);
+      assert.equal(rm.getAllometricScaling(), 11.11321976546847);
+      
+      var rm = motionsplan.Strength(300, 150);
+      assert.equal(rm.getAllometricScaling(), 10.626585691826113);
+    });
+  });
+  describe('getBodyweightPercent', function() {
+    it('should return the correct number', function() {
+      // weigt, reps
+      var rm = motionsplan.Strength(405, 220);
+      assert.equal(rm.getBodyweightPercent(), 1.8409090909090908);
+      
+      var rm = motionsplan.Strength(300, 150);
+      assert.equal(rm.getBodyweightPercent(), 2);
+    });
+  });
 });
