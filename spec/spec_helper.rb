0542e551afb14418d773041ace4dcedcd85c5ee5
Lars Olesen <lars@intraface.dk>
2020-04-16 07:18:10 +0000

    More

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,5 @@
+# https://medium.com/espark-engineering-blog/testing-your-jekyll-site-the-ruby-way-ccfa386a8318
+
 # Require all of the necessary gems
 require 'rspec'
 require 'capybara/rspec'
@@ -5,7 +7,6 @@ require 'rack/jekyll'
 require 'rack/test'
 require 'pry'
 require 'webdrivers'
-
 RSpec.configure do |config|
   config.expect_with :rspec do |expectations|
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
@@ -15,39 +16,8 @@ RSpec.configure do |config|
   end
   # Configure Capybara to use Selenium.
   Capybara.register_driver :selenium do |app|
-    chrome_prefs = {
-      'download' => {
-        'default_directory' => './support/',
-        'prompt_for_download' => false
-      },
-      'profile' => {
-        'default_content_settings' => { 'multiple-automatic-downloads': 1 }, # for chrome version olde ~42
-        'default_content_setting_values' => { 'automatic_downloads': 1 }, # for chrome newe 46
-        'password_manager_enabled' => false
-      },
-      'safebrowsing' => {
-        'enabled' => false,
-        'disable_download_protection' => true
-      }
-    }
-  
-    # Set headless with docker friendly args.
-    chrome_args = %w[window-size=1024,768 disable-gpu no-sandbox disable-translate no-default-browser-check disable-popup-blocking]
-    # To run full browser instead of headless mode, run this command: HEADLESS=false rspec spec
-    unless ENV.fetch('HEADLESS', 'true') == 'false'
-      chrome_args += %w[headless]
-    end
-  
-    # Initialize chromedriver.
-    capabilities = Selenium::WebDriver::Remote::Capabilities.chrome(
-      chromeOptions: {
-        prefs: chrome_prefs,
-        args: chrome_args
-      }
-    )
-    driver = Capybara::Selenium::Driver.new(app, browser: :chrome, desired_capabilities: capabilities)
-  
-    driver
+    # Configure selenium to use Chrome.
+    Capybara::Selenium::Driver.new(app, :browser => :chrome)
   end
   # Configure Capybara to load the website through rack-jekyll.
   # (force_build: true) builds the site before the tests are run,
